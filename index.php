459a1d516dcf984c79285e2045adfaa993ae46ff
Pedro Ventura <pedrojventura@gmail.com>
2012-10-26 17:26:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 $consumerKey    = 'Consumer-Key';
 $consumerSecret = 'Consumer-Secret';
 $oAuthToken     = 'OAuthToken';
@@ -8,9 +7,9 @@ $oAuthSecret    = 'OAuth Secret';
 # API OAuth
 require_once('twitteroauth.php');
 
-$tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
+$Tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
 
 # your code to retrieve data goes here, you can fetch your data from a rss feed or database
 
-$tweet->post('statuses/update', array('status' => 'here the content of your tweet, you can add hashtags or links'));
+$Tweet->post('statuses/update', array('status' => 'here the content of your tweet, you can add hashtags or links'));
 ?>
