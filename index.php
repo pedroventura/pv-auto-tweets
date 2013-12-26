cf2b7fad308c25c1dfb3e954ccee745fa937e59b
Pedro Ventura <pedrojventura@gmail.com>
2013-12-26 15:42:44 +0100

    debugin variable definition

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ $oAuthSecret    = 'OAuth Secret';
 # API OAuth
 require_once('twitteroauth.php');
 
-$Tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
+$tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
 
 # your code to retrieve data goes here, you can fetch your data from a rss feed or database
 
-$Tweet->post('statuses/update', array('status' => 'here the content of your tweet, you can add hashtags or links'));
+$tweet->post('statuses/update', array('status' => 'here the content of your tweet, you can add hashtags or links'));
 ?>
